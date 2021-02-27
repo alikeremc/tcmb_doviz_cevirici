81c3dd9022cc502071e58c03cbef77383e05c64a
alicevik <cevikali83@yahoo.com>
2021-02-27 11:43:48 +0300

    Döviz çevir.py dosyası eklendi.

--- doviz_cevir.py
+++ doviz_cevir.py
@@ -1,3 +1,18 @@
-print("Merhaba Dolar/Euro")
+#TCMB Sitesinden efektif dolar satış fiyatını çekelim
 
-print("Merhaba GitHub")
+from bs4 import *
+
+import requests
+
+sayfa=requests.get("https://www.turkiye.gov.tr/doviz-kurlari")
+print(sayfa.text)
+
+soup=BeautifulSoup(sayfa.text, "html.parser")
+
+efektif_dolar_satis=soup.find('td',text='1 ABD DOLARI')\
+    .find_next_sibling('td').\
+    find_next_sibling('td').\
+    find_next_sibling('td').\
+    find_next_sibling('td').text
+
+print("Dolar Satış",efektif_dolar_satis)
